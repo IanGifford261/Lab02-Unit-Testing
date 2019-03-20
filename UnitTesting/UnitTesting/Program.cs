55bb1495231e6a6fa07ba972102e09c5f544066a
iangifford <lightyagamie10@gmail.com>
2019-03-20 10:26:45 -0700

    Just a beginning commit, trying to think out my process

---
+++ UnitTesting/UnitTesting/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace UnitTesting
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to the Bank");
+
+            Console.WriteLine("View balance");
+            Console.WriteLine("Withdraw Money");
+            Console.WriteLine("Deposit Money");
+            Console.ReadLine();
+
+        }
+        /// <summary>
+        /// starting
+        /// </summary>
+        /// <returns></returns>
+        public static decimal viewBalance() 
+    }
+}
