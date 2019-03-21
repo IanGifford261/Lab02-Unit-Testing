f2802e4139c5b91d90de6ed8931a2555470dcd76
iangifford <lightyagamie10@gmail.com>
2019-03-20 20:33:15 -0700

    most functionality acheived. global variable "totalBalance" does not update. will fix at a later time

--- UnitTesting/UnitTesting/Program.cs
+++ UnitTesting/UnitTesting/Program.cs
@@ -29,7 +29,7 @@ namespace UnitTesting
                     int convertStgToInt = Convert.ToInt32(userInput);
                     if (convertStgToInt == 1)
                     {
-                        Console.WriteLine($"Your Current Balance is {totalBalance}");
+                        Console.WriteLine($"Your Current Balance is {totalBalance}");                       
 
                     }
                     else if (convertStgToInt == 2)
@@ -42,14 +42,14 @@ namespace UnitTesting
                     else if (convertStgToInt == 3)
                     {
                         Console.WriteLine($"Enter an amount you would like to deposit, your current balance is {totalBalance}");
-                        decimal userDeposit = decimal.Parse(Console.ReadLine());
-                        //decimal depositAmt = Convert.ToInt32(userDeposit);
-                        DepositAmt(userDeposit, totalBalance);
-                        Console.WriteLine($"Your balance is now {totalBalance}");
+                        string userDeposit = Console.ReadLine();
+                        decimal depositAmt = Convert.ToInt32(userDeposit);
+                        DepositAmt(depositAmt, totalBalance);
+                        
                     }
                     else
                     {
-
+                            
                     }
 
                 } while (runApp);
@@ -73,21 +73,25 @@ namespace UnitTesting
 
             } else
             {
-                decimal newBalance;
-                newBalance = totalBalance - withdrawnAmt;
-                return Convert.ToString(newBalance);
+                totalBalance = totalBalance - withdrawnAmt;
+                Console.WriteLine($"You new balance is now {totalBalance}");
+                return Convert.ToString(totalBalance);
+
             }
 
         }
 
-        public static string DepositAmt(decimal userDeposit, decimal totalBalance)
+        public static string DepositAmt(decimal depositAmt, decimal totalBalance)
         {
-            if (userDeposit > 0)
+            if (depositAmt > 0)
             {
-                decimal newBalance;
-                newBalance = userDeposit + totalBalance;
-                return Convert.ToString(newBalance);
-            } else
+
+                totalBalance = totalBalance + depositAmt;
+                Console.WriteLine($"You new balance is now {totalBalance}");
+                return Convert.ToString(totalBalance);
+
+            }
+            else
             {
                 return "You cannot deposit less than zero!";
 
